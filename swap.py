2b312ebf43e724c4f1b05e8a684b9cb05a5d9889
Rachit Kalal <117704684+kalalRachit@users.noreply.github.com>
2025-08-06 12:41:17 +0530

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,14 +1,16 @@
-x=10
-y=5
+# Python program to swap two variables
 
-print("Before swaping")
-print("X",x)
-print("Y",y)
+x = 5
+y = 10
 
-temp=x
-x=y
-y=temp
+# To take inputs from the user
+#x = input('Enter value of x: ')
+#y = input('Enter value of y: ')
 
-print("AFter Swaping")
-print("X",x)
-print("y",y)
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
